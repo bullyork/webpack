1f36eca7e5cefe3ae0db50e31b6d9b1cc04da77f
张雁峰 <zhangyanfeng@ezbuy.com>
2016-07-25 17:20:22 +0800

    增加kit

--- util/kit.js
+++ util/kit.js
@@ -1,14 +1,5 @@
 import moment from 'moment'
 
-function toTimestamp(v) {
-  // to 10 位 (精确到 秒)
-  return moment(v).format('X')
-}
-function  numberConnect(a,b) {
-  a = a.toString()
-  b = b.toString()
-  return a+b
-}
 function showTimestampToDate(ts) {
   ts = String(ts)
 
@@ -22,72 +13,98 @@ function showTimestampToDate(ts) {
 
   return moment(ts).format('MM-DD')
 }
-function showTimestamp(ts, fmt) {
+
+function toTimestamp(v) {
+  return moment(v).format('x')
+}
+function getPictureUrl(picture){
+  if(picture.indexOf('http')!= -1){
+    return 'url('+picture+')'
+  }else{
+    return 'url('+'http:'+picture+')'
+  }
+}
+function minTimestampToMs(ts){
   ts = String(ts)
-
-  if (ts.length !== 10 && ts.length !== 13) {
-    return '未知'
-  }
-
-  if (ts.length === 10) {
-    ts = parseInt(ts) * 1000
-  }
-
-  // WARN: Table 中 render: showTimestamp 会导致 第二个参数传入 record
-  // 所以, 不要用 ES6 默认参数处理 fmt
-  if (!(typeof fmt === 'string' && fmt.startsWith('YYYY'))) {
-    fmt = 'YYYY-MM-DD HH:mm'
-  }
-
-  return moment(ts).format(fmt)
+  return ts.length == 10 ? parseInt(ts) * 1000 : parseInt(ts)
+}
+//index 为当前所选，order为要插入的排序
+function arrayInsert(index, order, list){
+  const temp = list[index]
+  list.splice(index, 1)
+  list.splice(order, 0, temp)
+  return list
 }
 
-function getRequest() {
-  
-  const url = location.search; //获取url中"?"符后的字串
-   const theRequest = new Object();
-   if (url.indexOf("?") != -1) {
-      const str = url.substr(1);
-      const strs = str.split("&");
-      for(let i = 0; i < strs.length; i ++) {
-         theRequest[strs[i].split("=")[0]]=(strs[i].split("=")[1]);
-      }
-   }
-   return theRequest;
-}
-
-
-function equal(objA, objB)
-{
-    let flag = true
-    for (let key in objA) {
-      if(!objB.hasOwnProperty(key) || objB[key]!= objA[key]){
-        flag = false
-      }
+function uniqueSort(array){
+  let left = []
+  let right = []
+  for (let i = 0; i < array.length; i++) {
+    if(array[i].disabled){
+      right.push(array[i])
+    }else{
+      left.push(array[i])
     }
-    for (let key in objB) {
-      if(!objA.hasOwnProperty(key) || objA[key]!= objB[key]){
-        flag = false
-      }
+  }
+  return Array.concat([],left,right)
+}
+
+function toNormalDateString(date){
+  const dateArray = date.toLocaleDateString().split('/')
+  let [a,b,c] = dateArray
+  if(a.length == 4){
+    b = b.length < 2?'0'+b:b
+    c = c.length < 2?'0'+c:c
+    return a+b+c
+  }else{
+    b = b.length < 2?'0'+b:b
+    a = a.length < 2?'0'+a:a
+    return c+a+b
+  }
+}
+
+function normalDataStringToTime(date){
+  const year = Number(date.slice(0,4))
+  const month = Number(date.slice(4,6))-1
+  const day = Number(date.slice(6,8))
+  return new Date(year,month,day)
+}
+function distinguishCashAndRegister(list){
+  let cashList = []
+  let registerList = []
+  for (let i = 0; i < list.length; i++) {
+    if(list[i].duration == 0){
+      cashList.push(list[i])
+    }else{
+      registerList.push(list[i])
     }
-    return flag
+  }
+  return {
+    cashList,
+    registerList
+  }
 }
-
-function constraintImg(){
-  // console.info(document.getElementsByTagName('img'))
-  const ImgList = document.getElementsByTagName('img')
-  Array.prototype.map.call(ImgList,function(item){
-    const width = item.width
-    item.height = width
-  })
-
-}
+function getRequest() { 
+  var url = location.search; //获取url中"?"符后的字串 
+  var theRequest = new Object(); 
+  if (url.indexOf("?") != -1) { 
+    var str = url.substr(1); 
+    var strs = str.split("&"); 
+    for(var i = 0; i < strs.length; i ++) { 
+      theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]); 
+    } 
+  } 
+  return theRequest; 
+} 
 export {
+  distinguishCashAndRegister,
   toTimestamp,
-  numberConnect,
+  minTimestampToMs,
+  getPictureUrl,
+  arrayInsert,
+  uniqueSort,
+  toNormalDateString,
+  normalDataStringToTime,
   showTimestampToDate,
-  showTimestamp,
-  getRequest,
-  equal,
-  constraintImg
+  getRequest
 }
